f8921f3817863b28ae93c813bf043cad52ea8d42
Haruka Kai <plalal@gmail.com>
2016-04-11 23:57:20 +0900

    d parse

---
+++ qr/d/d.scm
@@ -0,0 +1,54 @@
+(use gauche.parameter)
+(use util.match)
+(use gauche.sequence)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; common library for GCJ ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (line-read :optional [reader read])
+  (with-input-from-string (read-line) reader))
+
+(define line-read$ (pa$ pa$ line-read))
+
+(define (replist n proc)
+  (let loop [[n n]
+             [acc '()]
+             ]
+    (if (zero? n)
+      (reverse acc)
+      (loop (- n 1) (cons (proc) acc)))))
+
+(define replist$ (pa$ pa$ replist))
+
+(define (read-matrix rows cols :optional [reader read-char])
+  (replist rows (line-read$ (replist$ cols reader))))
+
+(define gcj-current-case (make-parameter #f))
+
+(define (standard-formatter . xs) (string-join (map x->string xs) " "))
+
+(define (gcj-interact parser solver :optional [formatter standard-formatter])
+  (dotimes (n (line-read))
+    (parameterize [[gcj-current-case (+ n 1)]]
+      (format #t "Case #~a: ~a\n"
+              (gcj-current-case)
+              ((compose formatter
+                        solver
+                        parser))
+              ))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; problem specific code ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (main args)
+  (gcj-interact parse solve))
+
+(define (parse)
+  (line-read (replist$ 3 read))
+  )
+
+(define (solve . args)
+  (error "not implemented")
+  )
