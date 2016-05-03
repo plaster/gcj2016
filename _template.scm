62a3e1b7c08239c09787e3e645cfac5411e2158d
Haruka Kai <plalal@gmail.com>
2016-05-03 16:31:10 +0900

    now template use library

--- _template.scm
+++ _template.scm
@@ -1,56 +1,12 @@
-;; running on Gauche 0.9.4 http://practical-scheme.net/gauche/
+;; runs on Gauche 0.9.4: http://practical-scheme.net/gauche/
+;; module 'codejam' is available at:
+;;   https://github.com/plaster/google-code-jam-solutions/blob/master/lib/codejam.scm
+;; latest template is available at:
+;;   https://github.com/plaster/google-code-jam-solutions/blob/master/_template.scm
 
-(use gauche.parameter)
+(use codejam)
 (use util.match)
 (use gauche.sequence)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; common library for GCJ ;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (line-read :optional [reader read])
-  (with-input-from-string (read-line) reader))
-
-(define line-read$ (pa$ pa$ line-read))
-
-(define (replist n proc)
-  (let loop [[n n]
-             [acc '()]
-             ]
-    (if (zero? n)
-      (reverse acc)
-      (loop (- n 1) (cons (proc) acc)))))
-
-(define replist$ (pa$ pa$ replist))
-
-(define (read-matrix rows cols :optional [reader read-char])
-  (replist rows (line-read$ (replist$ cols reader))))
-
-(define current-case (make-parameter #f))
-
-(define (run parser solver emitter)
-  (dotimes (n (line-read))
-    (parameterize [[current-case (+ n 1)]]
-      ((.$ emitter solver parser)))))
-
-(define (run/parallel parser solver emitter :optional [parallel-level 4])
-  (let* [[n (line-read)]
-         [parsed (make-vector n #f)]
-         [solved (make-vector n #f)]
-         ]
-    (dotimes (i n)
-      (call-with-values parser
-                        (cut set! (vector-ref parsed i) <...>))
-      )
-    ;;; TODO: run solver parallel and store result to vector solved
-    (dotimes (i n)
-      (parameterize [[current-case (+ i 1)]]
-        (apply emitter (vector-ref solved i))
-        ))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; problem specific code ;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (main args)
   (run parse solve emit))
